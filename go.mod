85e1b86253f7d75ed3e1c929a0c04a8e6160132b
clyu <1016338804@qq.com>
2022-04-22 11:17:10 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/clyuz/sse/v2
+
+go 1.17
+
+require (
+	github.com/stretchr/testify v1.7.1
+	gopkg.in/cenkalti/backoff.v1 v1.1.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20220421235706-1d1ef9303861 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
---
+++ stream.go
@@ -0,0 +1,128 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package sse
+
+import "sync/atomic"
+
+// Stream ...
+type Stream struct {
+	event           chan *Event
+	quit            chan struct{}
+	register        chan *Subscriber
+	deregister      chan *Subscriber
+	subscribers     []*Subscriber
+	Eventlog        EventLog
+	subscriberCount int32
+	// Enables replaying of eventlog to newly added subscribers
+	AutoReplay   bool
+	isAutoStream bool
+}
+
+// newStream returns a new stream
+func newStream(bufsize int, replay, isAutoStream bool) *Stream {
+	return &Stream{
+		AutoReplay:   replay,
+		subscribers:  make([]*Subscriber, 0),
+		isAutoStream: isAutoStream,
+		register:     make(chan *Subscriber),
+		deregister:   make(chan *Subscriber),
+		event:        make(chan *Event, bufsize),
+		quit:         make(chan struct{}),
+		Eventlog:     make(EventLog, 0),
+	}
+}
+
+func (str *Stream) run() {
+	go func(str *Stream) {
+		for {
+			select {
+			// Add new subscriber
+			case subscriber := <-str.register:
+				str.subscribers = append(str.subscribers, subscriber)
+				if str.AutoReplay {
+					str.Eventlog.Replay(subscriber)
+				}
+
+			// Remove closed subscriber
+			case subscriber := <-str.deregister:
+				i := str.getSubIndex(subscriber)
+				if i != -1 {
+					str.removeSubscriber(i)
+				}
+
+			// Publish event to subscribers
+			case event := <-str.event:
+				if str.AutoReplay {
+					str.Eventlog.Add(event)
+				}
+				for i := range str.subscribers {
+					str.subscribers[i].connection <- event
+				}
+
+			// Shutdown if the server closes
+			case <-str.quit:
+				// remove connections
+				str.removeAllSubscribers()
+				return
+			}
+		}
+	}(str)
+}
+
+func (str *Stream) close() {
+	str.quit <- struct{}{}
+}
+
+func (str *Stream) getSubIndex(sub *Subscriber) int {
+	for i := range str.subscribers {
+		if str.subscribers[i] == sub {
+			return i
+		}
+	}
+	return -1
+}
+
+// addSubscriber will create a new subscriber on a stream
+func (str *Stream) addSubscriber(eventid int) *Subscriber {
+	atomic.AddInt32(&str.subscriberCount, 1)
+	sub := &Subscriber{
+		eventid:    eventid,
+		quit:       str.deregister,
+		connection: make(chan *Event, 64),
+	}
+
+	if str.isAutoStream {
+		sub.removed = make(chan struct{}, 1)
+	}
+
+	str.register <- sub
+	return sub
+}
+
+func (str *Stream) removeSubscriber(i int) {
+	atomic.AddInt32(&str.subscriberCount, -1)
+	close(str.subscribers[i].connection)
+	if str.subscribers[i].removed != nil {
+		str.subscribers[i].removed <- struct{}{}
+		close(str.subscribers[i].removed)
+	}
+	str.subscribers = append(str.subscribers[:i], str.subscribers[i+1:]...)
+}
+
+func (str *Stream) removeAllSubscribers() {
+	for i := 0; i < len(str.subscribers); i++ {
+		close(str.subscribers[i].connection)
+		if str.subscribers[i].removed != nil {
+			str.subscribers[i].removed <- struct{}{}
+			close(str.subscribers[i].removed)
+		}
+	}
+	atomic.StoreInt32(&str.subscriberCount, 0)
+	str.subscribers = str.subscribers[:0]
+}
+
+func (str *Stream) getSubscriberCount() int {
+	return int(atomic.LoadInt32(&str.subscriberCount))
+}
